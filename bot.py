0978fc8eabe4a2ce2a8f9f81d843d702b4c6642e
Felipe Faria <felipe.a.b.faria@gmail.com>
2020-04-07 19:01:48 -0300

    Adding dotenv lib

--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 # bot.py
 import os
-
 import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
